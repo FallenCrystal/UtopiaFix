1b546014bcbc861e0434b555393237d6692137c7
Akkariin <fallencrystal@outlook.com>
2024-06-02 21:41:34 +0800

    使用MethodHandles替代传统反射

--- src/main/java/net/miaomoe/twinklefix/trinkets/OverwrittenDefaultTrinkets.java
+++ src/main/java/net/miaomoe/twinklefix/trinkets/OverwrittenDefaultTrinkets.java
@@ -11,6 +11,8 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.Field;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -24,15 +26,21 @@ public final class OverwrittenDefaultTrinkets implements Trinket {
         final Field modifyTarget = TrinketsApi.class.getDeclaredField("DEFAULT_TRINKET");
         modifyTarget.setAccessible(true);
         final Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
-        final Field theUnsafeField = unsafeClass.getDeclaredField("theUnsafe");
-        theUnsafeField.setAccessible(true);
-        final Object theUnsafe = theUnsafeField.get(null);
-
-        final Object staticFieldBase = unsafeClass.getMethod("staticFieldBase", Field.class).invoke(theUnsafe, modifyTarget);
-        final long staticFieldOffset = (long) unsafeClass.getMethod("staticFieldOffset", Field.class).invoke(theUnsafe, modifyTarget);
-        unsafeClass.getMethod("putObject", Object.class, long.class, Object.class).invoke(theUnsafe, staticFieldBase, staticFieldOffset, instance);
+        final Object theUnsafe = MethodHandles.privateLookupIn(unsafeClass, MethodHandles.lookup())
+                .findStaticGetter(unsafeClass, "theUnsafe", unsafeClass)
+                .invoke();
+        final Object staticFieldBase = invoke1(unsafeClass, theUnsafe, modifyTarget, "staticFieldBase",  Object.class);
+        final long staticFieldOffset = (long) invoke1(unsafeClass, theUnsafe, modifyTarget, "staticFieldOffset", long.class);
+        MethodHandles
+                .publicLookup()
+                .findVirtual(unsafeClass, "putObject", MethodType.methodType(void.class, Object.class, long.class, Object.class))
+                .invoke(theUnsafe, staticFieldBase, staticFieldOffset, instance);
         // dev.emi.trinkets.api.Trinket -> net.miaomoe.twinklefix.trinkets.OverwrittenDefaultTrinkets
         TwinkleFix.logger.info("[TwinkleFix] Modified: {}", modifyTarget.get(null));
+    }
+
+    private static Object invoke1(final Class<?> unsafeClass, final Object theUnsafe, final Field target, final String name, final Class<?> type) throws Throwable {
+        return MethodHandles.publicLookup().findVirtual(unsafeClass, name, MethodType.methodType(type, Field.class)).invoke(theUnsafe, target);
     }
 
     // 如果模组没有特别的Trinket设置, 那么首先检查Trinket自己是否允许脱下装备. 反之如果玩家在创造模式 则无论如何都允许强制脱下装备
